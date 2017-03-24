f121c880cee026f1e37cfbd5a148bd692677fcbb
IRSM <sm372181@gmail.com>
2017-03-24 09:59:21 +0900

    별 피라미드 만들기
    중첩 for문 응용

---
+++ 별찍기.c
@@ -0,0 +1,5 @@
+﻿1>------ 빌드 시작: 프로젝트: aaaaa, 구성: Debug x64 ------
+1>  소스.c
+1>  aaaaa.vcxproj -> C:\Users\dsm2017\documents\visual studio 2015\Projects\aaaaa\x64\Debug\aaaaa.exe
+1>  aaaaa.vcxproj -> C:\Users\dsm2017\documents\visual studio 2015\Projects\aaaaa\x64\Debug\aaaaa.pdb (Full PDB)
+========== 빌드: 성공 1, 실패 0, 최신 0, 생략 0 ==========

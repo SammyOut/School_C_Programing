3928d5ccb33deaa3370901df2cc2448284c703b6
IRSM <sm372181@gmail.com>
2017-04-03 14:34:02 +0900

    use two dimensional array

---
+++ 124p two-dimensional array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int A[3][2] = {{100, 100, {"90, 86}, {89, 98}};
+	int i, j;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 2; j++)
+			printf("%d ", A[i][j]);
+		printf("\n");
+	}
+	return 0;	
+}

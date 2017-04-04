335c35b8a828d21093169ea9622ef4a4e236aa5a
IRSM <sm372181@gmail.com>
2017-04-04 19:32:07 +0900

    %(num)s

---
+++ After_School_Program/use %ns.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("%5s%6s%6s\n", "name", "class", "grade");
+	printf("-----------------\n");
+	printf("%5s%6s%6s\n", "Ahn", "A", "3");
+	printf("%5s%6s%6s\n", "Jong", "B", "2");
+	printf("%5s%6s%6s", "Kim", "C", "1");
+	return 0;
+}

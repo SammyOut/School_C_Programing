380387f7dec4b4a7c947177d457e9a618f33567d
IRSM <sm372181@gmail.com>
2017-04-04 19:32:37 +0900

    use shift2

---
+++ After_School_Program/use shift2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int num = 15;
+	int result1 = num<<1;
+	int result2 = num<<2;
+	printf("result1 : %d\n", result1);
+	printf("result2 : %d", result2);
+	return 0;
+}

af325dd8c9735146de9a12ada2114fa73c041610
IRSM <sm372181@gmail.com>
2017-04-11 22:08:55 +0900

    absuolute value
    절대값 구하기

--- After_School_Program/absolute value.c
+++ After_School_Program/absolute value.c
@@ -4,6 +4,8 @@ int main(void)
 {
 	int num;
 	scanf("%d", &num);
-	num >= 0? printf("%d", num) : printf("%d", -num);
+	//num >= 0? printf("%d", num) : printf("%d", -num);
+	num = (num>=0)? num : -num;
+	printf("%d", num);
 	return 0;
 }
